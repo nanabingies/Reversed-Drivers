78fe5f48274ac7fa0a8921430bf47e45e1b0d345
Emmanuel Sasu <nanabingies1@gmail.com>
2023-03-30 17:23:21 +0000

    Update Fxn_DriverRoutines.c

--- Vulnerable Drivers/stuxnet Rootkit/Disassembled/Fxn_DriverRoutines.c
+++ Vulnerable Drivers/stuxnet Rootkit/Disassembled/Fxn_DriverRoutines.c
@@ -137,3 +137,13 @@ ULONG GetRoutinesAddress() {
 
 	return (&dword_14128);
 }
+
+
+void NotifyRoutine(_In_opt_ PUNICODE_STRING FullImageName, _In_ HANDLE ProcessId, _In_ PIMAGE_INFO ImageInfo) {
+	if (KeGetCurrentIrql() > APC_LEVEL)		return;
+
+	if (ProcessId == 0)		return;
+
+	PloadImageNotifyRoutine(FullImageName, ProcessId, ImageInfo);
+	return;
+}
