e189c24ff5f125491a6a9565b70113cfa398e28b
Emmanuel Sasu <nanabingies1@gmail.com>
2023-06-08 20:58:42 +0000

    Update DispatchRoutines.c

--- Vulnerable Drivers/XHUNTER/Disassembled Driver Codes/DispatchRoutines.c
+++ Vulnerable Drivers/XHUNTER/Disassembled Driver Codes/DispatchRoutines.c
@@ -1,4 +1,4 @@
-#pragma once
+
 
 auto Fn_IRP_MJ_CREATE(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp) -> NTSTATUS {
 	InterlockedIncrement((volatile long*)dword_14000A498);
@@ -29,30 +29,30 @@ auto Fn_IRP_MJ_WRITE(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp) -> NTSTATU
 	auto io_stack_location = IoGetCurrentIrpStackLocation(Irp);
 	xigncode_write_request* xwr = reinterpret_cast<xigncode_write_request*>(Irp->AssociatedIrp.SystemBuffer);
 	NTSTATUS nt_status = STATUS_UNSUCCESSFUL;
-	BYTE var_318[0x300];
+	BYTE var_318[0x300]{};
 	PVOID var_320, var_330;
+	uint32_t val = 0x0;
 
 	if ((io_stack_location->Parameters.Write.Length != 0x270) || (xwr->size != 0x270) || (xwr->magic_num != 0x345821AB)) {
 		goto end;
 	}
 
 	sub_140001BD4(xwr, &var_318);
-	var_330 = sub_1400036F0(xwr->output_buffer_addr, 0x2FA, &var_320);
+	var_330 = sub_1400036F0(xwr->output_buffer_addr, val, &var_320);
 	if (var_330 == nullptr) {
 		goto end;
 	}
 
 	int count = 5;
-    unsigned char* ptr = var_318;
-    
-	do {
-		
-	} while (count > 0);
+	RtlCopyMemory(var_330, var_318, 0x300);
 
+	nt_status = STATUS_SUCCESS;
+	sub_14000359C(&var_320, var_330);
+	val = 0x2FA;
 
 end:
 	Irp->IoStatus.Status = nt_status;
-	Irp->IoStatus.Information = 0;
+	Irp->IoStatus.Information = val;
 	IoCompleteRequest(Irp, IO_NO_INCREMENT);
 
 	return nt_status;
