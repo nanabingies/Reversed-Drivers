fc5a4a1f42e4c9fa7e3a91730c9c83021597bd16
Emmanuel Sasu <nanabingies1@gmail.com>
2023-06-12 18:18:47 +0000

    Update Fn_GetKernelFunctionsAddress.c

--- Vulnerable Drivers/XHUNTER/Disassembled Driver Codes/Fn_GetKernelFunctionsAddress.c
+++ Vulnerable Drivers/XHUNTER/Disassembled Driver Codes/Fn_GetKernelFunctionsAddress.c
@@ -1,1 +1,22 @@
 
+
+auto Fn_GetKernelFunctionsAddress() -> NTSTATUS {
+	UNICODE_STRING usString = { 0 };
+
+	RtlInitUnicodeString(&usString, L"ObGetFilterVersion");
+	kernelUtils::_ObGetFilterVersion = reinterpret_cast<pObGetFilterVersion>(MmGetSystemRoutineAddress(&usString));
+
+	RtlInitUnicodeString(&usString, L"ObRegisterCallbacks");
+	kernelUtils::_ObRegisterCallbacks = reinterpret_cast<pObRegisterCallbacks>(MmGetSystemRoutineAddress(&usString));
+
+	RtlInitUnicodeString(&usString, L"ObUnRegisterCallbacks");
+	kernelUtils::_ObUnRegisterCallbacks = reinterpret_cast<pObUnRegisterCallbacks>(MmGetSystemRoutineAddress(&usString));
+
+	RtlInitUnicodeString(&usString, L"ExfAcquirePushLockExclusive");
+	kernelUtils:: = reinterpret_cast<pExfAcquirePushLockExclusive>(MmGetSystemRoutineAddress(&usString));
+
+	RtlInitUnicodeString(&usString, L"ExfReleasePushLockExclusive");
+	kernelUtils::_ObRegisterCallbacks = reinterpret_cast<pObRegisterCallbacks>(MmGetSystemRoutineAddress(&usString));
+
+	return STATUS_SUCCESS;
+}
