56e8a064138e182153140ab367a93c83d7915062
Emmanuel Sasu <nanabingies1@gmail.com>
2023-06-12 22:10:21 +0000

    Update Fn_DriverUnload.c

--- Vulnerable Drivers/XHUNTER/Disassembled Driver Codes/Fn_DriverUnload.c
+++ Vulnerable Drivers/XHUNTER/Disassembled Driver Codes/Fn_DriverUnload.c
@@ -1,6 +1,6 @@
 
 VOID Fn_DriverUnload(_In_ PDRIVER_OBJECT DriverObject) {
-	Fn_RemoveCallbacks();
+	Fn_RemoveProcessCallbacks();
 	IoDeleteSymbolicLink(&xmmword_14000A500);
 	sub_14000429C(&xmmword_14000A4F0);
 	sub_14000429C(&xmmword_14000A500);
